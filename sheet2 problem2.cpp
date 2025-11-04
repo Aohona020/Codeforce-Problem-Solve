fa8416ac9f154787b6d1e5483b97290acd1eef43
Aohona020 <naimaafrinaohona@gmail.com>
2025-11-04 23:15:34 +0600

    Add files via upload

---
+++ sheet2 problem2.cpp
@@ -0,0 +1,20 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    if (n < 2) {
+        cout << -1 << endl;
+    } else {
+        for (int i = 2; i <= n; i += 2) {
+            cout << i << endl;
+        }
+    }
+
+    return 0;
+}
+
