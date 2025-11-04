2962f7e6dae2a021dda749526cd8678138c91b41
Aohona020 <naimaafrinaohona@gmail.com>
2025-11-04 23:16:43 +0600

    Add files via upload

---
+++ sheet2 problem4.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    while (true) {
+        cin >> n;
+        if (n == 1999) {
+            cout << "Correct" << endl;
+            break;
+        } else {
+            cout << "Wrong" << endl;
+        }
+    }
+    return 0;
+}
